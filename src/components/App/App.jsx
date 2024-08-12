da3430f9c0868b76fa513ab49f4b60cb1038ef9c
Kseniya <159527696+Kseniya-Igorivna@users.noreply.github.com>
2024-08-12 18:13:16 +0300

    poiu

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,11 +1,14 @@
 import Profile from "../Profile/Profile";
-import userData from "../../userData.json";
 import FriendList from "../FriendList/FriendList";
+import TransactionHistory from "../TransactionHistory/TransactionHistory";
+
+import userData from "../../userData.json";
 import friends from "../../friends.json";
+import transactions from "../../transactions.json";
 
 export default function App() {
   return (
-    <div>
+    <>
       <Profile
         name={userData.username}
         tag={userData.tag}
@@ -13,7 +16,10 @@ export default function App() {
         image={userData.avatar}
         stats={userData.stats}
       />
+      <hr />
       <FriendList friends={friends} />
-    </div>
+      <hr />
+      <TransactionHistory items={transactions} />
+    </>
   );
 }
